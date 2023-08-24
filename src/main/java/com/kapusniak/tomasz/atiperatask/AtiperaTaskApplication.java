d32729ef9f14d3518906da5040764f11859b3af7
whitesnakeit <tomasz.kapusniak.it@gmail.com>
2023-08-24 14:26:35 +0200

    Initial commit

---
+++ src/main/java/com/kapusniak/tomasz/atiperatask/AtiperaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kapusniak.tomasz.atiperatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapusniak/tomasz/atiperatask/AtiperaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapusniak.tomasz.atiperatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
